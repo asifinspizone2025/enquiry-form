4eeaee6971542b7e2a0801b0ac016a9f9415a361
asifinspizone2025 <mohammad.asif@inspizone.co.in>
2025-03-24 14:56:13 +0530

    Update embed.js
    captcha script remove

--- embed.js
+++ embed.js
@@ -132,7 +132,7 @@
                 margin-top: 10px;
             }
         </style>
-         <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+         
     `;
 
     const container = document.getElementById('node-form');
